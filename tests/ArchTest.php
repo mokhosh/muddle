ab9f533a7fae1b2009f1212981abacbe0c1cfd5c
Mo Khosh <mskhoshnazar@gmail.com>
2024-11-01 16:25:55 +0330

    add arch tests

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,5 +1,7 @@
 <?php
 
-arch('it will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
-    ->each->not->toBeUsed();
+arch()->preset()->php();
+arch()->preset()->security();
+arch()->preset()->laravel();
+arch()->preset()->relaxed();
+arch()->preset()->strict();
