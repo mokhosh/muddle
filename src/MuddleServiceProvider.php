9f964d0afce2fdbc447a5df330c55a3e7ed7f432
Mo Khosh <mskhoshnazar@gmail.com>
2024-11-01 09:56:51 +0330

    cleanup

--- src/MuddleServiceProvider.php
+++ src/MuddleServiceProvider.php
@@ -10,16 +10,10 @@ class MuddleServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('muddle')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_muddle_table')
             ->hasCommand(MuddleCommand::class);
     }
 }
