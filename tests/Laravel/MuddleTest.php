56e9e61db84f2c67506bcf1e0fa6a2a720fe70e5
Mo Khosh <mskhoshnazar@gmail.com>
2024-11-01 16:00:35 +0330

    test default strategy config

--- tests/Laravel/MuddleTest.php
+++ tests/Laravel/MuddleTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Config;
 use Mokhosh\Muddle\Facades\Muddle;
 use Mokhosh\Muddle\Strategies\Text;
 use Mokhosh\Muddle\Strategies\Link;
@@ -8,6 +9,20 @@ it('works with basic unsafe strategies', function () {
     expect(Muddle::text('test@example.com'))
         ->toBe('test@example.com')
         ->and(Muddle::link('test@example.com'))
+        ->toBe('<a href="mailto:test@example.com">test@example.com</a>');
+});
+
+it('gets strategies from config', function () {
+    Config::set('muddle.strategy.text', Text\UnsafeEntities::class);
+    Config::set('muddle.strategy.link', Link\UnsafeEntities::class);
+
+    expect($entitizedText = Muddle::text('test@example.com'))
+        ->not->toBe('test@example.com')
+        ->and(html_entity_decode($entitizedText))
+        ->toBe('test@example.com')
+        ->and($entitizedLink = Muddle::link('test@example.com'))
+        ->not->toBe('<a href="mailto:test@example.com">test@example.com</a>')
+        ->and(html_entity_decode($entitizedLink))
         ->toBe('<a href="mailto:test@example.com">test@example.com</a>');
 });
 
