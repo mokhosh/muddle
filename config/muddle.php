8c3ebe6c47a9c9f811a953930aa5c70302ef8089
Mo Khosh <mskhoshnazar@gmail.com>
2024-11-01 14:27:31 +0330

    wishful config

--- config/muddle.php
+++ config/muddle.php
@@ -1,6 +1,18 @@
 <?php
 
-// config for Mokhosh/Muddle
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Strategy
+    |--------------------------------------------------------------------------
+    |
+    | Set default strategies for obfuscating text and email links
+    |
+    */
+    'strategy' => [
+        'text' => TextStrategy::class,
+        'link' => LinkStrategy::class,
+    ],
 
 ];
