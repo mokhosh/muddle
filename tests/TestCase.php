bcde7bc68fb035a77078d8955c05cb227f298efa
Mo Khosh <mskhoshnazar@gmail.com>
2024-11-01 16:20:56 +0330

    cleanup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,35 +2,15 @@
 
 namespace Mokhosh\Muddle\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Mokhosh\Muddle\MuddleServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Mokhosh\\Muddle\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
             MuddleServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_muddle_table.php.stub';
-        $migration->up();
-        */
     }
 }
